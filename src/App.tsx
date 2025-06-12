c1e368ffb76db60fbd73462a08729eff499f14e3
RickyBv1 <rickycortes668668@gmail.com>
2025-06-11 21:53:21 -0400

    feat: implement question fetching and update quiz start logic

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import { Container, Stack, Typography } from "@mui/material";
 import "./App.css";
 import { JavaScriptLogo } from "./JavaScriptLogo";
 import { Start } from "./Start";
+import { useQuestionsStore } from "./store/questions";
 
 function App() {
+  const questions = useQuestionsStore((state) => state.questions);
+
   return (
     <main>
       <Container maxWidth="sm">
@@ -19,7 +22,8 @@ function App() {
           </Typography>
         </Stack>
 
-        <Start />
+        {questions.length === 0 && <Start />}
+        {questions.length > 0 && <h1>Quiz Started!</h1>}
       </Container>
     </main>
   );
--- src/store/questions.ts
+++ src/store/questions.ts
@@ -4,5 +4,26 @@ import { type Question } from "../types.d";
 interface State {
   questions: Question[];
   currentQuestion: number;
-  fetchQuestions: (limit: number) => void;
+  fetchQuestions: (limit: number) => Promise<void>;
 }
+
+export const useQuestionsStore = create<State>((set, get) => {
+  return {
+    questions: [],
+    currentQuestion: 0,
+
+    fetchQuestions: async (limit: number) => {
+      set({
+        questions: [
+          {
+            id: 1,
+            question: "¿Cuál es la salida de este código?",
+            code: "console.log(typeof NaN)",
+            answers: ["undefined", "NaN", "string", "number"],
+            correctAnswer: 3,
+          },
+        ],
+      });
+    },
+  };
+});
