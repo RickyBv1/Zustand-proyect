a7b97ae567ba5065f9f13703bdc80d5af5679c7c
RickyBv1 <rickycortes668668@gmail.com>
2025-06-11 21:17:36 -0400

    feat: add MUI components and JavaScript logo to the app
    - Updated package.json to include MUI and Emotion dependencies.
    - Modified App.tsx to use MUI's Container, Stack, and Typography for layout.
    - Created a new JavaScriptLogo component to display the JavaScript logo.
    - Updated main.tsx to import Roboto font styles.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { Container, Stack, Typography } from "@mui/material";
 import "./App.css";
+import { JavaScriptLogo } from "./JavaScriptLogo";
 
 function App() {
-  return <></>;
+  return (
+    <main>
+      <Container maxWidth="sm">
+        <Stack
+          direction="row"
+          gap={2}
+          alignContent="center"
+          justifyContent="center"
+        >
+          <JavaScriptLogo />
+          <Typography variant="h2" component="h1">
+            JavaScript Quiz
+          </Typography>
+        </Stack>
+      </Container>
+    </main>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "@fontSource/roboto/300.css";
+import "@fontSource/roboto/400.css";
+import "@fontSource/roboto/500.css";
+import "@fontSource/roboto/700.css";
+
+createRoot(document.getElementById("root")!).render(<App />);
