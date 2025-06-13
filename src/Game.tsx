461d11556eea7c521c930dde8438fa42694efe8f
RickyBv1 <rickycortes668668@gmail.com>
2025-06-12 21:44:55 -0400

    feat: enhance Question component with answer list and styling improvements

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,4 +1,11 @@
-import { Card, Typography } from "@mui/material";
+import {
+  Card,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Typography,
+} from "@mui/material";
 import { useQuestionsStore } from "./store/questions";
 import SintaxHighlither from "react-syntax-highlighter";
 import { atomOneDarkReasonable } from "react-syntax-highlighter/dist/esm/styles/hljs";
@@ -6,7 +13,7 @@ import { type Question as QuestionType } from "./types";
 
 const Question = ({ info }: { info: QuestionType }) => {
   return (
-    <Card variant="outlined" sx={{ padding: 2, marginTop: 2 }}>
+    <Card variant="outlined" sx={{ bgcolor: "#222", padding: 2, marginTop: 2 }}>
       <Typography variant="h5">{info.question}</Typography>
 
       <SintaxHighlither
@@ -17,6 +24,16 @@ const Question = ({ info }: { info: QuestionType }) => {
       >
         {info.code}
       </SintaxHighlither>
+
+      <List sx={{ bgcolor: "#111" }} disablePadding>
+        {info.answers.map((answer, index) => (
+          <ListItem key={index} disablePadding divider>
+            <ListItemButton>
+              <ListItemText primary={answer} sx={{ textAlign: "center" }} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
     </Card>
   );
 };
